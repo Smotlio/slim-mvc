e690b99c39cfceafdac4568b277c517442aadf20
Smotlio <s.jordanov87@gmail.com>
2015-07-02 11:23:31 +0300

    folder structure

---
+++ app/config/settings.php
@@ -0,0 +1,19 @@
+<?php
+
+$settings = array(
+    'view' => new \Slim\Views\Twig(),
+    'templates.path' => '../app/Views',
+    'model' => (Object)array(
+            "message" => "Hello World"
+        )
+);
+
+return $settings;
+
+/*
+ * @todo
+ * 2) avtomati4no zarejdane na view-tata v kontrolera
+ * 3) vuzmojnost za dobavqne na prefix na kontroleri i view-ta
+ * 6) admin - gotovi template-i ot saita
+ * 7) iserver kato vendor biblioteka s vuzmojnost za slagane vuv composer
+ */
